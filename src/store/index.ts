08e0dcf9d86a75325a1168f957fb2cdf2bf7818f
노주훈 <joohoon.roh@gmail.com>
2024-07-05 18:50:23 +0900

    feat: 블루투스 기본 스토어 이전 준비 작업

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,17 @@
 import {configureStore, combineReducers} from '@reduxjs/toolkit';
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 import counter from './counter';
+import bluetooth from './bluetooth';
 
 const store = configureStore({
   reducer: combineReducers({
     counter: counter.reducer,
+    bluetooth: bluetooth.reducer,
   }),
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type StoreState = ReturnType<typeof store.getState>;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<StoreState> = useSelector;
 export default store;
