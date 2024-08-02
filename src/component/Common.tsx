639ee2eda8e3263358cd59630287d7701ca446a6
노주훈 <joohoon.roh@gmail.com>
2024-08-02 09:08:00 +0900

    feat: 작업중

--- src/component/Common.tsx
+++ src/component/Common.tsx
@@ -1,59 +1,137 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Button, Text, TouchableOpacity} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-interface childrenProps {
-  children: React.ReactNode;
-}
-
-const Body: React.FC<childrenProps> = ({children}) => {
-  return <View style={styles.Body}>{children}</View>;
+const Body: React.FC<{children: React.ReactNode}> = ({children}) => {
+  return <View style={styles.body}>{children}</View>;
 };
 
-const Container: React.FC<childrenProps> = ({children}) => {
-  return <View style={styles.Container}>{children}</View>;
+const Container: React.FC<{children: React.ReactNode}> = ({children}) => {
+  return <View style={styles.container}>{children}</View>;
 };
 
-const Title: React.FC<childrenProps> = ({children}) => {
+const Title: React.FC<{children: React.ReactNode; iconButton?: String; textButton?: String; clickEvent?: () => void}> = ({children, iconButton, textButton, clickEvent}) => {
   return (
-    <View style={styles.Title}>
-      <Text style={styles.TitleLeft}>{children}</Text>
+    <View style={styles.title}>
+      <Text style={styles.titleTitle}>{children}</Text>
+      <View style={styles.titleLeft}></View>
+      <View style={styles.titleRight}>
+        {iconButton && (
+          <TouchableOpacity style={styles.iconButton} onPress={clickEvent}>
+            <MaterialIcons name={String(iconButton)} size={24} style={styles.iconButtonIcon} />
+          </TouchableOpacity>
+        )}
+        {textButton && (
+          <TouchableOpacity style={styles.textButton} onPress={clickEvent}>
+            <Text style={styles.textButtonText}>{textButton}</Text>
+          </TouchableOpacity>
+        )}
+      </View>
     </View>
   );
 };
 
-const List: React.FC<childrenProps> = ({children}) => {
+const List: React.FC<{children: React.ReactNode}> = ({children}) => {
+  return <View style={styles.list}>{children}</View>;
+};
+
+const Item: React.FC<{children: String; subtitle?: String; iconButton?: String; textButton?: String; clickEvent?: () => void}> = ({children, subtitle, iconButton, textButton, clickEvent}) => {
   return (
-    <View style={styles.Title}>
-      <Text style={styles.TitleLeft}>{children}</Text>
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <Text style={styles.itemtitle}>{children}</Text>
+        <Text style={styles.itemSubtitle}>{subtitle}</Text>
+      </View>
+      <View style={styles.itemRight}>
+        {iconButton && (
+          <TouchableOpacity style={styles.iconButton} onPress={clickEvent}>
+            <MaterialIcons name={String(iconButton)} size={24} style={styles.iconButtonIcon} />
+          </TouchableOpacity>
+        )}
+        {textButton && (
+          <TouchableOpacity style={styles.textButton} onPress={clickEvent}>
+            <Text style={styles.textButtonText}>{textButton}</Text>
+          </TouchableOpacity>
+        )}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  Body: {
+  body: {
     flex: 1,
     gap: 36,
     paddingVertical: 12,
     backgroundColor: '#101811',
   },
-  Container: {
+  container: {
     gap: 10,
     backgroundColor: '#101811',
   },
-  Title: {
+  iconButton: {
+    alignItems: 'center',
+  },
+  iconButtonIcon: {
+    color: '#ffffff',
+  },
+  textButton: {
+    backgroundColor: '#254731',
+    paddingHorizontal: 12,
+    borderRadius: 6,
+  },
+  textButtonText: {
+    color: '#ffffff',
+    lineHeight: 24,
+    fontWeight: 'bold',
+  },
+  title: {
     flexDirection: 'row',
     gap: 10,
     paddingHorizontal: 24,
     paddingVertical: 6,
   },
-  TitleLeft: {
+  titleLeft: {
     flex: 1,
+  },
+  titleTitle: {
     color: '#ffffff',
     fontWeight: 'bold',
     fontSize: 16,
     lineHeight: 24,
   },
-  List: {},
+  titleRight: {
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  list: {
+    gap: 6,
+  },
+  item: {
+    flexDirection: 'row',
+    gap: 10,
+    paddingHorizontal: 24,
+    paddingVertical: 2,
+  },
+  itemLeft: {
+    flex: 1,
+  },
+  itemRight: {
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  itemtitle: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  itemSubtitle: {
+    color: '#98B0A1',
+    fontWeight: 'bold',
+    fontSize: 14,
+    lineHeight: 16,
+  },
 });
 
-export {Body, Container, Title, List};
+export {Body, Container, Title, List, Item};
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
-import {Button, View, Text, StyleSheet} from 'react-native';
-import {useDispatch, useSelector} from 'react-redux';
-import {increment, decrement} from '@/store/counter';
-import {StoreState} from '@/store';
-import {Body, Container, Title} from '@/component/Common';
+import {Body, Container, Title, List, Item} from '@/component/Common';
 
 const Home: React.FC = () => {
-  const dispatch = useDispatch();
-  const count = useSelector((state: StoreState) => state.counter);
-
   return (
     <Body>
       <Container>
         <Title>검색 리스트</Title>
-        <Text>Home Screen</Text>
-        <Text>{count}</Text>
-        <Button title="Increment" onPress={() => dispatch(increment())} />
-        <Button title="Decrement" onPress={() => dispatch(decrement())} />
+        <List>
+          <Item
+            textButton="연결"
+            subtitle="UUID : 2877188290"
+            clickEvent={() => {
+              console.log('dddd');
+            }}>
+            식물 재배 큐브 1
+          </Item>
+          <Item
+            textButton="연결"
+            subtitle="UUID : 2877188291"
+            clickEvent={() => {
+              console.log('dddd');
+            }}>
+            식물 재배 큐브 2
+          </Item>
+          <Item
+            textButton="연결"
+            subtitle="UUID : 2877188292"
+            clickEvent={() => {
+              console.log('dddd');
+            }}>
+            식물 재배 큐브 3
+          </Item>
+        </List>
       </Container>
     </Body>
   );
